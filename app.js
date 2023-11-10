ed59491bbd202ff3fd9f2f10725da7e561adeab2
Hubert Machała <hubert.machala@o2.pl>
2023-11-11 00:29:29 +0100

    Fix: debuging new endpoint

--- app.js
+++ app.js
@@ -31,25 +31,9 @@ const io = socketIo(server, { cors: SETTINGS.SOCKET_IO.CORS });
 // TODO: trzeba będzie lepiej zabezpieczyć ten endpoint, np przekazując token (bezpośrednio z endpointa logowania, przed wysłaniem zwrotki o pomyslnym zalogowaniu z tokenami)
 app.post('/disconnect/:userId', async (req, res) => {
     const userId = req.params.userId;
-    const socket = await getSocketIdByUserId(userId)
+    const socket = getSocketIdByUserId(userId)
 
-    const allData = myCache.data;
-    let result = null;
-
-    for (const key in allData) {
-        if (key.startsWith("connection_")) {
-            const socketId = key.replace("connection_", "");
-            const userValue = allData[key].v;
-
-            logToFile(`connected - id=${userValue} - ${socketId}`)
-
-            if(userValue == userId) {
-                result = socketId
-            }
-        }
-    }
-
-    logToFile(`TEST - id=${userId} - ${result}`)
+    logToFile(`TEST - id=${userId} - ${socket}`)
 
     if(socket) {
         // socket.emit('messageToAll', { type: 'forceLogout' }) // ewentualnie można coś jeszcze wyemitować, żeby odłączyć stare połączenie z socketem tego usera realtime (i obsłużyć w apce)
