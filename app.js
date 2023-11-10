64233d5043403c53b4d1ae59757949ba0bf814d0
Hubert Machała <hubert.machala@o2.pl>
2023-11-11 00:23:03 +0100

    Fix: debuging new endpoint

--- app.js
+++ app.js
@@ -33,7 +33,23 @@ app.post('/disconnect/:userId', async (req, res) => {
     const userId = req.params.userId;
     const socket = await getSocketIdByUserId(userId)
 
-    logToFile(`TEST - id=${userId} - ${getSocketIdByUserId(userId)}`)
+    const allData = myCache.data;
+    let result = null;
+
+    for (const key in allData) {
+        if (key.startsWith("connection_")) {
+            const socketId = key.replace("connection_", "");
+            const userValue = allData[key].v;
+
+            logToFile(`connected - id=${userValue} - ${socketId}`)
+
+            if(userValue === userId) {
+                result = socketId
+            }
+        }
+    }
+
+    logToFile(`TEST - id=${userId} - ${result}`)
 
     if(socket) {
         // socket.emit('messageToAll', { type: 'forceLogout' }) // ewentualnie można coś jeszcze wyemitować, żeby odłączyć stare połączenie z socketem tego usera realtime (i obsłużyć w apce)
