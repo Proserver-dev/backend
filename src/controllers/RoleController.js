d89aee32d63f29f7d467e5cc04f2d569476c827b
Hubert Machała <hubert.machala@o2.pl>
2023-11-29 22:46:00 +0100

    Docs: docs for /roles endpoints

--- src/controllers/RoleController.js
+++ src/controllers/RoleController.js
@@ -38,7 +38,7 @@ const getRoles = async (req, res) => {
 
     #swagger.responses[500] = {
         description: 'Błąd serwerowy',
-        schema: { error: 'ERR_GET_ROLES' }
+        schema: { error: 'ERR_INTERNAL_SERVER_ERROR' }
     }
 
     */
@@ -49,7 +49,7 @@ const getRoles = async (req, res) => {
         const roles = await Role.findAll();
         res.json(roles);
     } catch (error) {
-        res.status(API_RESULTS.ERR_GET_ROLES.status_code).json({ error: API_RESULTS.ERR_GET_ROLES.code });
+        res.status(API_RESULTS.ERR_INTERNAL_SERVER_ERROR.status_code).json({ error: API_RESULTS.ERR_INTERNAL_SERVER_ERROR.code });
     } 
 }
 
@@ -69,7 +69,7 @@ const getOneRole = async (req, res) => {
 
     #swagger.responses[500] = {
         description: 'Błąd serwerowy',
-        schema: { error: 'ERR_GET_ROLES' }
+        schema: { error: 'ERR_INTERNAL_SERVER_ERROR' }
     }
 
     */
@@ -85,7 +85,7 @@ const getOneRole = async (req, res) => {
             }
         }
     } catch (error) {
-        res.status(API_RESULTS.ERR_GET_ROLES.status_code).json({ error: API_RESULTS.ERR_GET_ROLES.code });
+        res.status(API_RESULTS.ERR_INTERNAL_SERVER_ERROR.status_code).json({ error: API_RESULTS.ERR_INTERNAL_SERVER_ERROR.code });
     } 
 }
 
@@ -98,6 +98,35 @@ const addRole = async (req, res) => {
         TokenAuth: []
     }]
 
+    #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: {
+            name: "Testowa rola"
+        }
+    }
+
+    #swagger.responses[400] = { 
+        description: "Musisz przekazać nazwę roli - pole name",
+        schema: {
+            error: ['ERR_PROVIDE_NAME_FIELD', 'ERR_PROVIDE_LOGIN_TOKEN']
+        }  
+    }
+
+    #swagger.responses[403] = { 
+        description: "Nazwa roli jest zarezerwowana albo nie masz uprawnień",
+        schema: {
+            error: ['ERR_ROLE_NAME_IS_RESERVED', 'ERR_ADMIN_PRIVILEGES_REQUIRED']
+        }  
+    }
+
+    #swagger.responses[409] = { 
+        description: "Taka rola już istnieje",
+        schema: {
+            error: 'ERR_ROLE_ALREADY_EXISTS'
+        }  
+    }
+
     #swagger.responses[201] = {
         description: 'Wszystko poszło GIT',
         schema: { 
@@ -108,33 +137,33 @@ const addRole = async (req, res) => {
 
     #swagger.responses[500] = {
         description: 'Błąd serwerowy',
-        schema: { error: 'ERR_CREATE_ROLE' }
+        schema: { error: 'ERR_INTERNAL_SERVER_ERROR' }
     }
 
     */
 
     saveLogFromEndpointRequest(req)
-
-    const name = req.body.name;
-    const short = generateSlug(name);
-
-    if(!name) {
-        return res.status(API_RESULTS.ERR_PROVIDE_NAME_FIELD.status_code).json({ error: API_RESULTS.ERR_PROVIDE_NAME_FIELD.code });
-    }
-  
-    if (isRoleBlocked(name)) {
-        return res.status(API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.code });
-    }
-
-    if (!(await isShortFieldUnique(short))) {
-        return res.status(API_RESULTS.ERR_ROLE_ALREADY_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_ALREADY_EXISTS.code });
-    }
   
     try {
-      const role = await Role.create({ name, short });
-      res.status(API_RESULTS.SUCCESS_CREATED_ROLE.status_code).json({ success: API_RESULTS.SUCCESS_CREATED_ROLE.code, role });
+        const name = req.body.name;
+        const short = generateSlug(name);
+    
+        if(!name) {
+            return res.status(API_RESULTS.ERR_PROVIDE_NAME_FIELD.status_code).json({ error: API_RESULTS.ERR_PROVIDE_NAME_FIELD.code });
+        }
+      
+        if (isRoleBlocked(name)) {
+            return res.status(API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.code });
+        }
+    
+        if (!(await isShortFieldUnique(short))) {
+            return res.status(API_RESULTS.ERR_ROLE_ALREADY_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_ALREADY_EXISTS.code });
+        }
+
+        const role = await Role.create({ name, short });
+        res.status(API_RESULTS.SUCCESS_CREATED_ROLE.status_code).json({ success: API_RESULTS.SUCCESS_CREATED_ROLE.code, role });
     } catch (error) {
-        res.status(API_RESULTS.ERR_CREATE_ROLE.status_code).json({ error: API_RESULTS.ERR_CREATE_ROLE.code });
+        res.status(API_RESULTS.ERR_INTERNAL_SERVER_ERROR.status_code).json({ error: API_RESULTS.ERR_INTERNAL_SERVER_ERROR.code });
     }
 }
 
@@ -147,6 +176,34 @@ const editRole = async (req, res) => {
         TokenAuth: []
     }]
 
+    #swagger.responses[400] = { 
+        description: "Musisz przekazać nazwę roli - pole name",
+        schema: {
+            error: ['ERR_PROVIDE_NAME_FIELD', 'ERR_PROVIDE_LOGIN_TOKEN']
+        }  
+    }
+
+    #swagger.responses[403] = { 
+        description: "Nazwa roli jest zarezerwowana albo nie masz uprawnień",
+        schema: {
+            error: ['ERR_ROLE_NAME_IS_RESERVED', 'ERR_ADMIN_PRIVILEGES_REQUIRED']
+        }  
+    }
+
+    #swagger.responses[404] = { 
+        description: "Rola o takim id nie istnieje",
+        schema: {
+            error: ['ERR_ROLE_NOT_EXISTS', 'ERR_USER_FROM_TOKEN_NOT_EXISTS']
+        }  
+    }
+
+    #swagger.responses[409] = { 
+        description: "Rola o takiej nazwie już istnieje",
+        schema: {
+            error: 'ERR_ROLE_ALREADY_EXISTS'
+        }  
+    }
+
     #swagger.responses[200] = {
         description: 'Wszystko poszło GIT',
         schema: { 
@@ -157,48 +214,40 @@ const editRole = async (req, res) => {
 
     #swagger.responses[500] = {
         description: 'Błąd serwerowy',
-        schema: { error: 'ERR_EDIT_ROLE' }
+        schema: { error: 'ERR_INTERNAL_SERVER_ERROR' }
     }
 
     */
 
     saveLogFromEndpointRequest(req)
-
-    const name = req.query.name;
-
-    if(!name) {
-        return res.status(API_RESULTS.ERR_PROVIDE_NAME_FIELD.status_code).json({ error: API_RESULTS.ERR_PROVIDE_NAME_FIELD.code });
-    }
-
-    const short = generateSlug(name);
-    const role = await Role.findByPk(req.params.id);
-
-    if (!role) {
-        return res.status(API_RESULTS.ERR_ROLE_NOT_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_NOT_EXISTS.code });
-    }
-  
-    if (isRoleBlocked(role.name)) {
-        return res.status(API_RESULTS.ERR_ROLE_EDIT_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_EDIT_NAME_IS_RESERVED.code });
-    }
-
-    if (isRoleBlocked(name)) {
-        return res.status(API_RESULTS.ERR_ROLE_EDIT_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_EDIT_NAME_IS_RESERVED.code });
-    }
-
-    if(name != role.name && !(await isShortFieldUnique(short))) {
-        return res.status(API_RESULTS.ERR_ROLE_EDIT_ALREADY_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_EDIT_ALREADY_EXISTS.code });
-    }
   
     try {
-        const [updatedRows] = await Role.update({ name, short }, {
-            where: { id: req.params.id },
-        });
+        const name = req.query.name;
+
+        if(!name) {
+            return res.status(API_RESULTS.ERR_PROVIDE_NAME_FIELD.status_code).json({ error: API_RESULTS.ERR_PROVIDE_NAME_FIELD.code });
+        }
     
-        if (updatedRows === 0) {
+        const short = generateSlug(name);
+        const role = await Role.findByPk(req.params.id);
+    
+        if (!role) {
             return res.status(API_RESULTS.ERR_ROLE_NOT_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_NOT_EXISTS.code });
         }
+      
+        if (isRoleBlocked(role.name)) {
+            return res.status(API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.code });
+        }
+    
+        if (isRoleBlocked(name)) {
+            return res.status(API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.code });
+        }
+    
+        if(name != role.name && !(await isShortFieldUnique(short))) {
+            return res.status(API_RESULTS.ERR_ROLE_ALREADY_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_ALREADY_EXISTS.code });
+        }
 
-        const role = await Role.findByPk(req.params.id);
+        await role.update({ name, short })
 
         // TODO: tutaj kwestia, czy przy aktualizowaniu danych w bazie potrzebna będzie jakaś zwrotka jeśli się udało
         // jeśli nie, to można zrobić: res.status(204).send() i wtedy nie trzeba będzie pobierać ponownie aktualnych danych z bazy - będzie działało szybciej
@@ -206,7 +255,7 @@ const editRole = async (req, res) => {
 
         res.status(API_RESULTS.SUCCESS_EDIT_ROLE.status_code).json({ success: API_RESULTS.SUCCESS_EDIT_ROLE.code, role });
     } catch (error) {
-        res.status(API_RESULTS.ERR_EDIT_ROLE.status_code).json({ error: API_RESULTS.ERR_EDIT_ROLE.code });
+        res.status(API_RESULTS.ERR_INTERNAL_SERVER_ERROR.status_code).json({ error: API_RESULTS.ERR_INTERNAL_SERVER_ERROR.code });
     }
 }
 
@@ -219,6 +268,13 @@ const deleteRole = async (req, res) => {
         TokenAuth: []
     }]
 
+    #swagger.responses[403] = { 
+        description: "Nazwa roli jest zarezerwowana albo nie masz uprawnień",
+        schema: {
+            error: ['ERR_ROLE_NAME_IS_RESERVED', 'ERR_ADMIN_PRIVILEGES_REQUIRED']
+        }  
+    }
+
     #swagger.responses[200] = {
         description: 'Wszystko poszło GIT',
         schema: { 
@@ -229,24 +285,24 @@ const deleteRole = async (req, res) => {
 
     #swagger.responses[500] = {
         description: 'Błąd serwerowy',
-        schema: { error: 'ERR_DELETE_ROLE' }
+        schema: { error: 'ERR_INTERNAL_SERVER_ERROR' }
     }
 
     */
     
     saveLogFromEndpointRequest(req)
-
-    const role = await Role.findByPk(req.params.id);
-  
-    if (!role) {
-        return res.status(API_RESULTS.ERR_ROLE_NOT_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_NOT_EXISTS.code });
-    }
-  
-    if (isRoleBlocked(role.name)) {
-        return res.status(API_RESULTS.ERR_ROLE_DELETE_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_DELETE_NAME_IS_RESERVED.code });
-    }
   
     try {
+        const role = await Role.findByPk(req.params.id);
+
+        if (!role) {
+            return res.status(API_RESULTS.ERR_ROLE_NOT_EXISTS.status_code).json({ error: API_RESULTS.ERR_ROLE_NOT_EXISTS.code });
+        }
+      
+        if (isRoleBlocked(role.name)) {
+            return res.status(API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.status_code).json({ error: API_RESULTS.ERR_ROLE_NAME_IS_RESERVED.code });
+        }
+
         await Role.destroy({
             where: { id: req.params.id },
         });
@@ -257,7 +313,7 @@ const deleteRole = async (req, res) => {
 
         res.status(API_RESULTS.SUCCESS_DELETE_ROLE.status_code).json({ success: API_RESULTS.SUCCESS_DELETE_ROLE.code, role });
     } catch (error) {
-        res.status(API_RESULTS.ERR_DELETE_ROLE.status_code).json({ error: API_RESULTS.ERR_DELETE_ROLE.code });
+        res.status(API_RESULTS.ERR_INTERNAL_SERVER_ERROR.status_code).json({ error: API_RESULTS.ERR_INTERNAL_SERVER_ERROR.code });
     }
 }
 
