18264b95e53a5db51dbdde5cda92fe5bc8cc5dd9
Hubert Machała <hubert.machala@o2.pl>
2023-12-02 18:30:12 +0100

    Fix: change delay to 5s

--- src/socketio/Disconnect.js
+++ src/socketio/Disconnect.js
@@ -7,7 +7,7 @@ function delay(ms) {
 }
 
 async function disconnect(socket, currentUserId, myCache) {
-    await delay(2000);
+    await delay(5000);
     const user = await User.findByPk(currentUserId)
     const isLoggedIn = (user.loginToken !== null || user.loginToken !== "") // czasami przy rozłączeniu z tokenem user może być dalej zalogowany i posiadać ważny token
     socket.broadcast.emit(SOCKET_EVENTS.RECEIVE_CONNECTION_ACTION, { 
