940172b14151005c5c2e257280cecfb32ef2740a
Hubert Machała <hubert.machala@o2.pl>
2023-12-02 18:26:08 +0100

    Fix: add 2 secounds delay before socket disconnect event

--- src/socketio/Disconnect.js
+++ src/socketio/Disconnect.js
@@ -2,10 +2,20 @@ const { logToFile } = require('../../src/functions');
 const SOCKET_EVENTS = require('../constants/socketEvents');
 const User = require('../models/UserModel');
 
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function disconnect(socket, currentUserId, myCache) {
+    await delay(2000);
     const user = await User.findByPk(currentUserId)
     const isLoggedIn = (user.loginToken !== null || user.loginToken !== "") // czasami przy rozłączeniu z tokenem user może być dalej zalogowany i posiadać ważny token
-    socket.broadcast.emit(SOCKET_EVENTS.RECEIVE_CONNECTION_ACTION, { action: 'disconnected', userId: currentUserId, isLoggedIn: isLoggedIn, socketId: null })
+    socket.broadcast.emit(SOCKET_EVENTS.RECEIVE_CONNECTION_ACTION, { 
+        action: 'disconnected', 
+        userId: currentUserId, 
+        isLoggedIn: isLoggedIn, 
+        socketId: null 
+    })
     logToFile(`Socket.io - Klient rozłączony - socket_id: ${socket.id}, userId: ${currentUserId}`);
     myCache.del(`connection_${socket.id}`)
 }
